21ac5aef1401b5439875544f377678802831919b
irene <ireneru@alumnos.iesgalileo.es>
2023-01-12 13:08:35 +0100

    organizacion por carpetas

---
+++ controlador/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Principal</title>
+
+</head>
+<body>
+    <?php
+    session_start();
+
+    if(isset($_SESSION['usuario'])){
+        $usuario = $_SESSION['usuario'];
+        include "./vista/index_usuario.html";
+
+    }else {
+        include "./vista/index_noUsuario.html";;
+    }
+    
+    
+    ?>
+   
+</body>
+</html>
